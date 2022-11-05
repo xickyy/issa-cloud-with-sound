b5554100dd6329ac92b950911b11cbe9233c1b3b
ricky <ricky_moore@ymail.com>
2022-11-05 09:46:35 -0700

    added users migration table and seeds

--- authenticate-me/backend/db/seeders/20221104042254-demo-user.js
+++ authenticate-me/backend/db/seeders/20221104042254-demo-user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -12,6 +13,23 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+   await queryInterface.bulkInsert('Users', [
+    {
+      email: 'demo@user.io',
+      username: 'Demo-lition',
+      hashedPassword: bcrypt.hashSync('password')
+    },
+    {
+      email: 'user1@user.io',
+      username: 'FakeUser1',
+      hashedPassword: bcrypt.hashSync('password2')
+    },
+    {
+      email: 'user2@user.io',
+      username: 'FakeUser2',
+      hashedPassword: bcrypt.hashSync('password3')
+    }
+   ])
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +39,9 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete('Users', {
+      username: {[Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2']}
+    }, {});
   }
 };
