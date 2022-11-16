03bf3b2cdbf056d4775c7a876977a9ca8332274a
ricky <ricky_moore@ymail.com>
2022-11-15 21:18:41 -0800

    done with routes up to number 65 on the score card

--- authenticate-me/backend/routes/api/albums.js
+++ authenticate-me/backend/routes/api/albums.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Album } = require('../../db/models');
+const { Album, User, Song} = require('../../db/models');
 
 router.post('/', async (req, res) => {
   let {title, description, imageUrl} = req.body;
@@ -24,6 +24,21 @@ router.get('/', async (req, res) => {
 router.get('/current', async(req, res) => {
   const allCurrent = await Album.findAll({where: {userId: req.user.id}});
   res.json(allCurrent);
-})
+});
+
+router.get('/:albumId', async(req, res, next) => {
+  const reqId = req.params.albumId;
+  const album = await Album.findByPk(reqId);
+
+  if (album && album.id.toString() === reqId) {
+    const user = await User.findByPk(album.userId);
+    const song = await Song.findAll({where: {albumId: reqId}});
+    res.json({album, user, song});
+  } else {
+    const e = new Error('no such album exists with this id');
+    e.status = 404;
+    return next(e);
+  }
+});
 
 module.exports = router;
--- authenticate-me/backend/routes/api/artists.js
+++ authenticate-me/backend/routes/api/artists.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('../../db/models');
+const { User, Album } = require('../../db/models');
 
+router.get('/:userId/albums', async (req, res, next) => {
+  const userId = req.user.id;
+  const user = await User.findByPk(userId);
+  if(userId && user.id === userId) {
+    const userAlbums = await Album.findAll({where: {userId: userId}});
+    res.json(userAlbums);
+  } else {
+    const e = new Error('No such user exists with this id');
+    e.status = 404;
+    return next(e);
+  }
+
+});
 
 module.exports = router;
