10b6a72b5d393e2877a029901427cc44ea448fb6
ricky <ricky_moore@ymail.com>
2023-01-07 15:48:30 -0800

    refreshing the page no longer logs out the user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,7 +33,7 @@ export const login = (user) => async (dispatch) => {
 export const restoreUser = () => async dispatch => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
-  dispatch(setUser(data.user));
+  dispatch(setUser(data));
   return response;
 };
 
